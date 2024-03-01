feeef585b60bf674356075889e3bbcc1cadc9960
Kobe Dieryck <kobe.dieryck@icloud.com>
2024-03-01 10:24:46 +0100

    added example

---
+++ CUDA_device_properties.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "cuda.h"
+#include "cuda_runtime.h"﻿
+ 
+// Print device properties
+void printDevProp(cudaDeviceProp devProp)
+{
+    printf("Major revision number:         %d\n",  devProp.major);
+    printf("Minor revision number:         %d\n",  devProp.minor);
+    printf("Name:                          %s\n",  devProp.name);
+}
+ 
+int main()
+{
+    int devCount;
+    cudaGetDeviceCount(&devCount);
+    printf("There is/are %d CUDA devices on your system\r\n", devCount);
+ 
+    for (int i = 0; i < devCount; ++i)
+    {
+        // Get device properties
+        printf("\nCUDA Device #%d\n", i);
+        cudaDeviceProp devProp;
+        cudaGetDeviceProperties(&devProp, i);
+        printDevProp(devProp);
+    }
+    return 0;
+}
